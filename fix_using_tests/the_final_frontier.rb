ad9b77857e2ac49fab7e1fc33314bb140cffd4d8
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-05 20:42:16 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -9,7 +9,7 @@ def state_log(star_date)
 end
 
 def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+  hash = {"Geordi", "Data", "Worf", "William", "Beverly", "Deanna"}
 end
 
 def greet_crew (crew)
